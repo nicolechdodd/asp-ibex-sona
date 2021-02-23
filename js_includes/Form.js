bd0424e10d4ff7bb11e090450b7462f49e1d1905
nicolechdodd <nicolechdodd@gmail.com>
2021-02-23 07:28:06 -0800

    rtl config

--- js_includes/Form.js
+++ js_includes/Form.js
@@ -19,10 +19,10 @@ jqueryWidget: {
         this.saveReactionTime = dget(this.options, "saveReactionTime", false);
         this.obligatoryErrorGenerator =
             dget(this.options, "obligatoryErrorGenerator",
-                 function (field) { return "The \u2018" + field + "\u2019 field is obligatory."; }); // TODO translate 
+                 function (field) { return "حقل هذا إلزامي"; });
         this.obligatoryCheckboxErrorGenerator =
             dget(this.options, "obligatoryCheckboxErrorGenerator",
-                 function (field) { return "You must check the " + field + " checkbox to continue."; }); // TODO translate
+                 function (field) { return "يجب عليك تحديد خانة الاختيار للمتابعة"; });
         this.obligatoryRadioErrorGenerator =
             dget(this.options, "obligatoryRadioErrorGenerator",
                  function (field) { return "You must select an option for \u2018" + field + "\u2019."; });
